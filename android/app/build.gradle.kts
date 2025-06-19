6997c70de6e2fb90877846e4d813a569ea5461cb
davelee <daveli@mrcdevlab.com>
2025-06-19 16:12:48 +0800

    fix 打包失败

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,6 +38,10 @@ android {
             signingConfig = signingConfigs.getByName("debug")
             isMinifyEnabled = true
             isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
